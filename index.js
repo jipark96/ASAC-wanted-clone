a863fe25935674ea5369f1a84b26005af788ad74
jipark96 <wlgus996@naver.com>
2023-04-13 17:26:40 +0900

    4/13 오후 작업

--- index.js
+++ index.js
@@ -7,14 +7,6 @@ const goModal2 = document.getElementById("go_modal2");
 const signupModal2 = document.getElementById("signup_modal2");
 const closeBtn2 = document.getElementById("close2")
 
-// signupButton.addEventListener("click",()  => {
-//   signupModal.style.display = "block"; // 모달 열기
-// });
-
-// closeBtn.addEventListener("click",()  => {
-//   signupModal.style.display = "none"; // 모달 닫기
-// });
-
 signupButton.onclick = ()  => {
   signupModal.style.display = "block"; // 모달 열기
 
