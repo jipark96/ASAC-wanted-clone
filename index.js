412094f2c1c18d640ef1de255744e9015979d816
jipark96 <wlgus996@naver.com>
2023-04-13 22:43:29 +0900

    4/13

--- index.js
+++ index.js
@@ -1,44 +1,27 @@
-
 const signupButton = document.getElementById("signup_button");
 const signupModal = document.getElementById("signup_modal");
-const closeBtn = document.getElementById("close")
+const closeBtn = document.getElementById("close");
 
 const goModal2 = document.getElementById("go_modal2");
 const signupModal2 = document.getElementById("signup_modal2");
-const closeBtn2 = document.getElementById("close2")
+const closeBtn2 = document.getElementById("close2");
 
-signupButton.onclick = ()  => {
+signupButton.onclick = () => {
   signupModal.style.display = "block"; // 모달 열기
+};
+goModal2.onclick = () => {
+  signupModal.style.display = "none"; // 첫번째 모달 닫기
+  signupModal2.style.display = "block"; // 두번째 모달 열기
+};
 
-  goModal2.onclick = () => {
-    signupModal.style.display = "none"; // 첫번째 모달 닫기
-    signupModal2.style.display = "block"; // 두번째 모달 열기
-  };
-  
-  closeBtn2.onclick = ()  => {
-    signupModal2.style.display = "none"; // 두번째 모달 닫기
-    signupModal.style.display = "block"; // 첫번째 모달 열기
-  };
+closeBtn2.onclick = () => {
+  signupModal2.style.display = "none"; // 두번째 모달 닫기
+  signupModal.style.display = "block"; // 첫번째 모달 열기
+};
 
-  
-closeBtn.onclick = ()  => {
+closeBtn.onclick = () => {
   signupModal.style.display = "none"; // 모달 닫기
 };
-};
-
-
-
-
-//[체크박스, 전체 동의할 경우 다 체크하기]
-const modal2_chk_all = document.getElementById("modal2_chk_all");
-const modal2_chk = document.querySelectorAll(".modal2_checkbox");
-
-modal2_chk_all.addEventListener("change", () => {
-  modal2_chk.forEach((checkbox) => {
-    checkbox.checked = modal2_chk_all.checked;
-  });
-});
-
 
 //[이메일 형식이 올바르지 않을 경우 경고 표시하기]
 const emailInput = document.getElementById("email_input");
@@ -49,14 +32,23 @@ emailInput.oninput = () => {
   const email = emailInput.value;
   const emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
 
-  if(emailValid) {
+  if (emailValid) {
     emailInput.style.border = "1px solid #36f";
     emailError.style.display = "none";
     emailBtn.classList.add("active");
   } else {
     emailInput.style.border = "1px solid red";
     emailError.style.display = "block";
-    emailBtn.classList.remove("active")
+    emailBtn.classList.remove("active");
   }
 };
 
+//[체크박스, 전체 동의할 경우 다 체크하기]
+const modal2_chk_all = document.getElementById("modal2_chk_all");
+const modal2_chk = document.querySelectorAll(".modal2_checkbox");
+
+modal2_chk_all.addEventListener("change", () => {
+  modal2_chk.forEach((checkbox) => {
+    checkbox.checked = modal2_chk_all.checked;
+  });
+});
